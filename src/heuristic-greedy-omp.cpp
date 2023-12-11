d5cf23511cd0743c6ff5b680b95069595edf76d0
FelixLuciano <felix.lucianodias@gmail.com>
2023-12-11 10:01:40 -0300

    Refactor clique finding algorithm in heuristic-greedy-omp.cpp

--- src/heuristic-greedy-omp.cpp
+++ src/heuristic-greedy-omp.cpp
@@ -19,11 +19,9 @@ std::vector<unsigned int> getNodeMaximalClique(std::vector<std::vector<bool>>& g
 
             std::vector<bool> intersection(nVertices, 0);
 
-            for (unsigned int j = 0; j < nVertices; j++) {
-                if (j == node || j == i || graph[node][j] & graph[i][j]) {
+            for (unsigned int j = 0; j < nVertices; j++)
+                if (j == node || j == i || graph[node][j] & graph[i][j])
                     intersection[j] = 1;
-                }
-            }
 
             #pragma omp critical
             {
@@ -37,45 +35,56 @@ std::vector<unsigned int> getNodeMaximalClique(std::vector<std::vector<bool>>& g
         #pragma omp for schedule(static, 12)
         for (auto u = intersections.begin(); u != intersections.end(); ++u) {
             auto& u_vec = std::get<2>(*u);
+            unsigned int degree1 = 0;
 
             for (auto v = u+1; v != intersections.end(); ++v) {
-                unsigned int degree = 0;
+                if (!u_vec[std::get<0>(*v)]) continue;
+
                 auto& v_vec = std::get<2>(*v);
+                unsigned int degree2 = 0;
 
-                for (unsigned long i = 0; i < nVertices; i++) {
-                    if (u_vec[i] & v_vec[i]) {
-                        ++degree;
-                    }
-                }
+                for (unsigned long i = 0; i < nVertices; i++)
+                    if (u_vec[i] & v_vec[i])
+                        ++degree2;
+                
+                if (degree1 < degree2)
+                    degree1 = degree2;
 
-                if (degree > std::get<1>(*u)) {
-                    std::get<1>(*u) = degree;
-                }
+                if (std::get<1>(*v) < degree2)
+                    std::get<1>(*v) = degree2;
             }
+
+            if (std::get<1>(*u) < degree1)
+                std::get<1>(*u) = degree1;
         }
     }
 
-    std::sort(intersections.begin(), intersections.end(), [](const std::tuple<unsigned int, unsigned int, std::vector<bool>>& a, const std::tuple<unsigned int, unsigned int, std::vector<bool>>& b) {
+    std::sort(intersections.begin(), intersections.end(), [](const auto& a, const auto& b) {
         return std::get<1>(a) > std::get<1>(b);
     });
 
-    std::vector<bool> clique(nVertices, 1);
+    std::vector<bool> clique(nVertices);
 
-    for (auto row : intersections) {
-        if (clique[std::get<0>(row)] == 0)
-            continue;
+    for (auto u = intersections.begin(); u != intersections.end(); ++u) {
+        std::fill(clique.begin(), clique.end(), 1);
 
-        for (unsigned int i = 0; i < nVertices; i++)
-            clique[i] = clique[i] & std::get<2>(row)[i];
+        for (auto v = u+1; v != intersections.end(); ++v) {
+            if (clique[std::get<0>(*v)] == 0)
+                continue;
+
+            for (unsigned int i = 0; i < nVertices; i++)
+                clique[i] = clique[i] & std::get<2>(*v)[i];
+        }
+
+        if (clique[std::get<0>(*u)])
+            break;
     }
 
     std::vector<unsigned int> ids;
 
-    for (unsigned int i = 0; i < clique.size(); i++) {
-        if (clique[i] != 0) {
+    for (unsigned int i = 0; i < clique.size(); i++)
+        if (clique[i] != 0)
             ids.push_back(i);
-        }
-    }
 
     return ids;
 }
@@ -89,19 +98,16 @@ std::vector<unsigned int> getMaximalClique(std::vector<std::vector<bool>>& graph
         for (unsigned int i = 0; i < nVertices; i++) {
             unsigned int degree = 1;
 
-            for (bool n : graph[i]) {
+            for (bool n : graph[i])
                 degree += n;
-            }
 
-            if (degree < clique.size()) {
+            if (degree < clique.size())
                 continue;
-            }
 
             auto nodeClique = getNodeMaximalClique(graph, nVertices, i);
 
-            if (nodeClique.size() > clique.size()) {
+            if (nodeClique.size() > clique.size())
                 clique = nodeClique;
-            }
         }
     }
 
